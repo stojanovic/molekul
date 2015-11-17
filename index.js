276e5fdbb62fb89d92fb8dc681e829e2c6dcf98d
Slobodan Stojanovic <slobodan@cloudhorizon.com>
2015-11-17 09:30:50 +0100

    feat(slack): connect Slack oauth

--- index.js
+++ index.js
@@ -1,15 +1,56 @@
 'use strict'
 
-const Hapi = require('hapi')
+const Hapi   = require('hapi'),
+      qs     = require('qs'),
+      rp     = require('request-promise'),
+      config = require('./config/config.json')
 
 let server = new Hapi.Server()
 server.connection({ port: process.env.PORT || 3000 })
+
+// console.log(config.slack)
 
 server.route({
   method:  'GET',
   path:    '/',
   handler(req, reply) {
     reply('Molekul Slack bot')
+  }
+})
+
+server.route({
+  method:  'GET',
+  path:    '/connect',
+  handler(req, reply) {
+    let query = {
+      client_id:    config.slack.appClientId,
+      scope:        'channels:read chat:write:bot',
+      redirect_uri: config.slack.redirectUri
+    }
+    let authUrl = `https://slack.com/oauth/authorize?${qs.stringify(query)}`
+    
+    reply('Redirecting to Slack').redirect(authUrl)
+  }
+})
+
+server.route({
+  method: 'GET',
+  path:   '/connectRedirect',
+  handler(req, reply) {
+    if (req.query.code) {
+      let params = {
+        client_id:     config.slack.appClientId,
+        client_secret: config.slack.appSecret,
+        code:          req.query.code,
+        redirect_uri:  config.slack.redirectUri
+      }
+      let oauthUrl = `https://slack.com/api/oauth.access?${qs.stringify(params)}`
+      
+      rp(oauthUrl)
+        .then(response => reply(response))
+    } else {
+      reply('Code is required')
+    }
   }
 })
 
